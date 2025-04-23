b15cab5528605f3370e1592ffe29cbaa700115a2
MavSingh <86635253+Mav-Singh@users.noreply.github.com>
2025-04-23 13:57:49 -0700

    Update PW_Strength_checker_V2.py

--- PROJECTS/PW_Strength_checker_V2.py
+++ PROJECTS/PW_Strength_checker_V2.py
@@ -10,7 +10,8 @@ question = input("Do you want to know what is strong password looks like? y/n: "
 if question == 'y':
     print("Strong password is atleast 12 characters, with atleast 1 upper, 1 lower, 1 special character and 1 numeric value \n")
 else:
-    print("proceeding further. Loading..... \n")
+    print("\nproceeding further. Loading.....\n")
+
 #get the password input from the user
 pw = input("Enter your password: ")
 
@@ -25,7 +26,8 @@ lower = any(char.islower() for char in pw)
 digit = any(char.isdigit() for char in pw)
 
 #check special character
-special = any(char in string.punctuation for char in pw)
+special = any(char in string.punctuation for char in pw) # These chars are included !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~""
+
 
 #check length
 pw_len = len(pw)
@@ -35,9 +37,13 @@ if upper and lower and special and digit and pw_len >= 12:
     print("Strong Password")
 elif upper and lower and special and digit and 8< pw_len < 12:
     print("Moderate Password")
-elif upper and lower and special and digit and 4< pw_len < 8:
+elif upper and lower and special and digit and 4< pw_len <= 8:
     print("Weak Password")
 else:
-    print("Password didnt meet the requirments")
+    print("\nPassword didnt meet the requirments")
 
 #display why passord is weak, moderate or stronger
+if len(pw) < 12 and upper and lower and special and digit:
+    print("tip: make your password lengthier(at least 12 chars)")
+else:
+    print("\nYour password must have \n1. One Char in upper \n2. Atleast one char in lower \n3. Atleast one special char \n4. Atleast one digit \n5. Atleast 8 characters")
