6c2ec3e3f445d3babe418f6efd73e0dddc3a4f22
MavSingh <86635253+Mav-Singh@users.noreply.github.com>
2025-04-25 10:47:27 -0700

    Update PW_Checker_V3.py

--- PROJECTS/PW_Checker_V3.py
+++ PROJECTS/PW_Checker_V3.py
@@ -1,1 +1,8 @@
+ #Password must include the following:
+# Use between 8 and 16 characters
+# Include at least one lowercase (a-z) and one uppercase letter (A-Z)
+# Include at least one special character(e.g. !@#$&)
+# Does not contain blank spaces or the following special characters: < > ,
+# Include at least one digit (0-9)
+# Passwords match
 
