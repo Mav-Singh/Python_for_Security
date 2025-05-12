0bbb32290a0b6b634ebb2c7d801e9cf07d9fcc13
MavSingh <86635253+Mav-Singh@users.noreply.github.com>
2025-05-12 12:50:37 -0700

    Update PW_Checker_V3.py

--- PROJECTS/PW_Checker_V3.py
+++ PROJECTS/PW_Checker_V3.py
@@ -6,3 +6,20 @@
 # Include at least one digit (0-9)
 # Passwords match
 
+
+#Import modules
+import random
+
+#Ask user if wants to generate a system generated strong password:
+password = []
+num = [1,2,3,4,5,6,7,8,9,0]
+char = ["!", "@", "#", "$", "%", "^", "&", "*","(",")"]
+low_alph = ["a","b","c"]
+up_alph = ["A", "B", "C"]
+ran = random.choice(char)
+print(ran)
+
+#Create own Password and check password strength:
+#pw = input("Please enter Your Password: ")
+
+#Keep on telling whats keeping the password weak or moderate
