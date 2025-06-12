63468753d6ae7b8f9c981e750eb4bd0180e1822b
MavSingh <86635253+Mav-Singh@users.noreply.github.com>
2025-06-11 22:52:44 -0700

    Update IP_Sweeping.py

--- PROJECTS/IP_Sweeping.py
+++ PROJECTS/IP_Sweeping.py
@@ -1,20 +1,23 @@
-#An IP sweeping Python script is typically used to identify active hosts (devices) on a network by scanning a range of IP addresses to see which ones respond. The purpose is often to gather network information for system administration, inventory, or cybersecurity tasks.
+# An IP sweeping Python script is typically used to identify active hosts (devices) on a network by scanning a range of IP addresses to see which ones respond. The purpose is often to gather network information for system administration, inventory, or cybersecurity tasks.
 
 # add ascii to display IP Sweeper
-ip_sweeper = "
-.___ __________    _________                                                 
-|   |\______   \  /   _____/__  _  __  ____    ____  ______    ____  _______ 
-|   | |     ___/  \_____  \ \ \/ \/ /_/ __ \ _/ __ \ \____ \ _/ __ \ \_  __ \
-|   | |    |      /        \ \     / \  ___/ \  ___/ |  |_> >\  ___/  |  | \/
-|___| |____|     /_______  /  \/\_/   \___  > \___  >|   __/  \___  > |__|   
-                         \/               \/      \/ |__|         \/       "
+ip_sweeper = '''
+ _                                                       
+(_)                                                      
+ _  _ __    ___ __      __  ___   ___  _ __    ___  _ __ 
+| || '_ \  / __|\ \ /\ / / / _ \ / _ \| '_ \  / _ \| '__|
+| || |_) | \__ \ \ V  V / |  __/|  __/| |_) ||  __/| |   
+|_|| .__/  |___/  \_/\_/   \___| \___|| .__/  \___||_|   
+   | |                                | |                
+   |_|                                |_|                    
+'''
 print(ip_sweeper)
-    
-#import modules
-import subprocess #this module lets you run commands
-import platform #to detect which OS i am on
 
-#detect the OS
+# import modules
+import subprocess  # this module lets you run commands
+import platform  # to detect which OS i am on
+
+# detect the OS
 if platform.system().lower == "windows":
     para = "-n"
 else:
@@ -25,12 +28,12 @@ base_ip = "192.168.1."
 start = 1
 end = 10
 
-#loop through Ips
-for ip in range(start, end+1):
+# loop through Ips
+for ip in range(start, end + 1):
     ip = base_ip + str(ip)
     print(ip)
 
-#run the command
+# run the command
 result = subprocess.run(["ping", para, "1", ip], stdout=subprocess.DEVNULL)
 if result.returncode == 0:
     alive_ips.append(ip)
