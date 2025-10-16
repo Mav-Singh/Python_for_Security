1bd58c2350216d1e96d53e6a7def6e84ba667dba
MavSingh <86635253+Mav-Singh@users.noreply.github.com>
2025-10-16 14:56:11 -0700

    Update pw_gen.py

--- PROJECTS/PW_Strength_with_GUI/pw_gen.py
+++ PROJECTS/PW_Strength_with_GUI/pw_gen.py
@@ -1,1 +1,16 @@
 
+import secrets
+import string
+
+def local_password(length=5, num=1, characters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{};:,.<>/?"):
+
+    passwords = []
+    for _ in range(num):
+        password = ''.join(secrets.choice(characters) for _ in range(length))
+        passwords.append(password)
+    return passwords
+
+# Example usage
+pw = local_password(10,1)
+# print(f"Randomly generatd password for you: {pw}")
+
